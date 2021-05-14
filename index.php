8549f7b95828f41f00687f8ae08f88054154141f
Eric Thomas <edt514@gmail.com>
2021-05-13 23:54:10 -0400

    Add challenge content

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+   
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Eric Thomas">
+	<meta name="description" content="Eric Thomas recreation of the DevChallenges recipes challenge">
+	<title> Recipes </title>
+	<link rel="shortcut icon" href="img/icon.jpg">    
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/recipes.css">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+   a
+</body>
+</html>
