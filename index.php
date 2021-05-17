af230a1d8399b3373fc87da38d51621bff258504
Eric Thomas <edt514@gmail.com>
2021-05-16 23:42:44 -0400

    Add some design elements

--- index.php
+++ index.php
@@ -9,12 +9,145 @@
 	<meta name="author" content="Eric Thomas">
 	<meta name="description" content="Eric Thomas recreation of the DevChallenges recipes challenge">
 	<title> Recipes </title>
-	<link rel="shortcut icon" href="img/icon.jpg">    
+	<link rel="shortcut icon" href="img/icon.png">    
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/recipes.css">
-	<script src="js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 </head>
 <body>
-   a
+    <section class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>
+                    Classic Cheesecake Recipe
+                </h1>
+            </div>
+            <div class="col-sm-12 col-md-6 description">
+                <img src="img/6dots.svg" class="icon">
+                <p>
+                    <em>
+                        Look no further for a creamy and ultra smooth classic cheesecake recipe! Paired with a buttery graham cracker crust, no one can deny its simple decadence. For the best results, bake in a water bath.
+                    </em>
+                </p>
+            </div>
+        </div> 
+        <div class="clear"> &nbsp; </div>
+        <div class="row">
+            <div class="col-sm-12">
+                <img src="img/cake.png" class="hero" alt="Cheesecake" title="A classic cheesecake topped with berries">
+            </div>
+        </div>
+        <div class="clear"> &nbsp; </div>
+        <div class="row instructions">
+            <div class="col-sm-12 col-md-10">
+                <div class="info">
+                    <ul>
+                        <li>
+                            <span class="material-icons icon orange">local_dining</span>
+                            <p class="subtitle">
+                                <span class="subtitle"> Yields </span>
+                                <span class="orange"> 12 servings </span>
+                            </p>                            
+                        </li>
+                        <li>
+                            <span class="material-icons icon">schedule</span>
+                        </li>
+                        <li>
+
+                        </li>
+                        <li>
+
+                        </li>
+                    </ul>
+                </div>
+                
+                <h2>
+                    Ingredients
+                </h2>
+                <form action="#" method="post">
+                    <p>
+                        <em class="subtitle">
+                            Graham Cracker Crust
+                        </em>
+                    </p>
+                    
+                    <ul>
+                        <li>
+                            <input type="checkbox" name="gcc-1">
+                            <label for="gcc-1">
+                                1 and 1/2 cups (150g) <strong> graham cracker crumbs </strong> (about 10 full sheet graham crackers)
+                            </label>
+                        </li>                        
+                        <li>
+                            <input type="checkbox" name="gcc-2">
+                            <label for="gcc-2">
+                                5 Tablespoons (70g) <strong>unsalted butter</strong>, melted
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="gcc-3">
+                            <label for="gcc-3">
+                                1/4 cup (50g) <strong> granulated sugar </strong>
+                            </label>
+                        </li>
+                    </ul>
+                    
+                    <p>
+                        <em class="subtitle">
+                            Cheesecake
+                        </em>
+                    </p>
+                    
+                    <ul>
+                        <li>
+                            <input type="checkbox" name="cc-1">
+                            <label for="cc-1">
+                                Four 8-ounce blocks (904g) full-fat <strong>cream cheese</strong>, softened to room temperature
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="cc-2">
+                            <label for="cc-2">
+                                1 cup (200g) <strong>granulated sugar</strong>strong>
+                            </label>
+                        </li>                        
+                        <li>
+                            <input type="checkbox" name="cc-3">
+                            <label for="cc-3">
+                                1 cup (240g) full-fat <strong>sour cream</strong> at room temperature
+                            </label>
+                        </li>                        
+                        <li>
+                            <input type="checkbox" name="cc-4">
+                            <label for="cc-4">
+                                1 teaspoon <strong>pure vanilla extract</strong>
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="cc-5">
+                            <label for="cc-5">
+                                2 teaspoons <strong>fresh lemon juice</strong> (optional, but recommended)
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="cc-6">
+                            <label for="cc-6">
+                                3 large <strong>eggs</strong> at room temperature
+                            </label>
+                        </li>
+                        <li>
+                            <input type="checkbox" name="cc-7">
+                            <label for="cc-7">
+                                Topping suggestions: <em> salted caramel, lemon curd, strawberry topping, chocolate ganache, red wine chocolate ganache, fresh fruit, whipped cream, or raspberry sauce </em>  (recipe in notes)
+                            </label>
+                        </li>
+                    </ul>
+                </form>
+            </div>
+        </div>
+    </section>
+    
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
